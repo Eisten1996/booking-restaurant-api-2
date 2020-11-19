c3f0b0ab7af71ef5d2f7a6b29687147501a70c1f
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-18 22:01:30 -0500

    Crear la clase principal de manejo de errores que extiende de Exception

---
+++ src/main/java/pe/dipper/bookingrestaurantapi/ErrorDto.java
@@ -0,0 +1,35 @@
+package pe.dipper.bookingrestaurantapi;
+
+import java.io.Serializable;
+
+/**
+ * @author Dipper
+ * @project booking-restaurant-api
+ * @created 18/11/2020 - 21:47
+ */
+public class ErrorDto implements Serializable {
+
+    private String name;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public ErrorDto(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/pe/dipper/bookingrestaurantapi/exceptions/BookingException.java
@@ -0,0 +1,42 @@
+package pe.dipper.bookingrestaurantapi.exceptions;
+
+import pe.dipper.bookingrestaurantapi.ErrorDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Dipper
+ * @project booking-restaurant-api
+ * @created 18/11/2020 - 21:44
+ */
+public class BookingException extends Exception {
+    private final String code;
+    private final int responseCode;
+    private final List<ErrorDto> errorDtoList = new ArrayList<>();
+
+    public BookingException(String code, int responseCode, String message) {
+        super(message);
+        this.code = code;
+        this.responseCode = responseCode;
+    }
+
+    public BookingException(String code, int responseCode, String message, List<ErrorDto> errorDtoList) {
+        super(message);
+        this.code = code;
+        this.responseCode = responseCode;
+        this.errorDtoList.addAll(errorDtoList);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public List<ErrorDto> getErrorDtoList() {
+        return errorDtoList;
+    }
+}
