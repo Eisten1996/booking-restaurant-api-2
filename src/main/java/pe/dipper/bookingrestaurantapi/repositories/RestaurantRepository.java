76d5364579c9cc1ff59ccc002e3f5ed5959a6b3d
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-18 21:15:55 -0500

    Creamos las consultas en nuestro repositorio de Restaurantes

--- src/main/java/pe/dipper/bookingrestaurantapi/repositories/RestaurantRepository.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/repositories/RestaurantRepository.java
@@ -1,9 +1,25 @@
 package pe.dipper.bookingrestaurantapi.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pe.dipper.bookingrestaurantapi.entities.Restaurant;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Dipper
  * @project booking-restaurant-api
  * @created 18/11/2020 - 17:03
  */
-public class RestaurantRepository {
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    @Override
+    Optional<Restaurant> findById(Long id);
+
+    Optional<Restaurant> findByName(String nameRestaurant);
+
+    @Query("SELECT REST from Restaurant REST")
+    public List<Restaurant> findRestaurants();
 }
