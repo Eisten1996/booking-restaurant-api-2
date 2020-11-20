13b1c3af4e26a3cb8ed96d9db27e6041989f8b7a
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-19 23:43:21 -0500

    Crear nuestras clases Json

---
+++ src/main/java/pe/dipper/bookingrestaurantapi/jsons/TurnRest.java
@@ -0,0 +1,34 @@
+package pe.dipper.bookingrestaurantapi.jsons;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Dipper
+ * @project booking-restaurant-api
+ * @created 19/11/2020 - 23:40
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TurnRest {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
