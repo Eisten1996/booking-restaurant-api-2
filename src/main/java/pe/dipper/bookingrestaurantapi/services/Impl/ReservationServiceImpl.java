9c73c5156f4883de5dbccce0fe8344ba3246f465
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-21 17:57:45 -0500

    Crear el Servicio de Realizar reserva - Setear en Reservation

--- src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/ReservationServiceImpl.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/ReservationServiceImpl.java
@@ -3,11 +3,15 @@ package pe.dipper.bookingrestaurantapi.services.Impl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import pe.dipper.bookingrestaurantapi.entities.Reservation;
+import pe.dipper.bookingrestaurantapi.entities.Restaurant;
+import pe.dipper.bookingrestaurantapi.entities.Turn;
 import pe.dipper.bookingrestaurantapi.exceptions.BookingException;
 import pe.dipper.bookingrestaurantapi.exceptions.NotFoundException;
 import pe.dipper.bookingrestaurantapi.jsons.CreateReservationRest;
 import pe.dipper.bookingrestaurantapi.jsons.ReservationRest;
 import pe.dipper.bookingrestaurantapi.repositories.ReservationRepository;
+import pe.dipper.bookingrestaurantapi.repositories.RestaurantRepository;
+import pe.dipper.bookingrestaurantapi.repositories.TurnRepository;
 import pe.dipper.bookingrestaurantapi.services.ReservationService;
 
 /**
@@ -20,6 +24,9 @@ public class ReservationServiceImpl implements ReservationService {
     @Autowired
     ReservationRepository reservationRepository;
 
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
     public static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
     @Override
@@ -29,6 +36,18 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public String createReservation(CreateReservationRest createReservationRest) throws BookingException {
+        final Restaurant restaurantId = restaurantRepository
+                .findById(createReservationRest
+                        .getRestaurantId())
+                .orElseThrow(() -> new NotFoundException("RESTAURANT_NOT_FOUND", "RESTAURANT_NOT_FOUND"));
+
+        String locator = generateLocator(restaurantId, createReservationRest);
+        final Reservation reservation = new Reservation();
+        reservation.setLocator(locator);
+        reservation.setPerson(createReservationRest.getPerson());
+        reservation.setDate(createReservationRest.getDate());
+        reservation.setRestaurant(restaurantId);
+
         return null;
     }
 
@@ -36,5 +55,11 @@ public class ReservationServiceImpl implements ReservationService {
         return reservationRepository
                 .findById(reservationId)
                 .orElseThrow(() -> new NotFoundException("SNOT-404-1", "RESERVATION_NOT_FOUND"));
+    }
+
+    private String generateLocator(
+            Restaurant restaurantId,
+            CreateReservationRest createReservationRest) throws BookingException {
+        return restaurantId.getName() + createReservationRest.getTurnId();
     }
 }
