dd3d1dc291c9daaa97ac9d7bcdd9ecd77bc00533
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-01 00:34:52 -0500

    Mockeando nuestro Servicio de restaurante que trae un restaurante por id

--- src/test/java/pe/dipper/bookingrestaurantapi/controller/RestaurantControllerTest.java
+++ src/test/java/pe/dipper/bookingrestaurantapi/controller/RestaurantControllerTest.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import pe.dipper.bookingrestaurantapi.controllers.RestaurantController;
 import pe.dipper.bookingrestaurantapi.exceptions.BookingException;
 import pe.dipper.bookingrestaurantapi.jsons.RestaurantRest;
+import pe.dipper.bookingrestaurantapi.jsons.TurnRest;
 import pe.dipper.bookingrestaurantapi.responses.BookingResponse;
 import pe.dipper.bookingrestaurantapi.services.RestaurantService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Dipper
@@ -21,9 +26,17 @@ import pe.dipper.bookingrestaurantapi.services.RestaurantService;
  */
 public class RestaurantControllerTest {
     private static final Long RESTAURANT_ID = 1L;
+    private static final String NAME = "Burger";
+    private static final String DESCRIPTION = "Todo tipo de Hamburguesa";
+    private static final String ADDRESS = "Av Peru";
+    private static final String IMAGE = "www.image.com";
+
     private static final String SUCCESS_STATUS = "Succes";
     private static final String SUCCESS_CODE = "200 OK";
     private static final String OK = "OK";
+
+    private static final List<TurnRest> TURN_LIST = new ArrayList<>();
+    private static final RestaurantRest RESTAURANT_REST = new RestaurantRest();
 
     @Mock
     RestaurantService restaurantService;
@@ -34,6 +47,14 @@ public class RestaurantControllerTest {
     @BeforeEach
     public void init() throws BookingException {
         MockitoAnnotations.initMocks(this);
+        RESTAURANT_REST.setName(NAME);
+        RESTAURANT_REST.setDescription(DESCRIPTION);
+        RESTAURANT_REST.setAddress(ADDRESS);
+        RESTAURANT_REST.setId(RESTAURANT_ID);
+        RESTAURANT_REST.setImage(IMAGE);
+        RESTAURANT_REST.setTurns(TURN_LIST);
+
+        Mockito.when(restaurantService.getRestaurantById(RESTAURANT_ID)).thenReturn(RESTAURANT_REST);
     }
 
     @Test
@@ -42,7 +63,7 @@ public class RestaurantControllerTest {
         assertEquals(response.getStatus(), SUCCESS_STATUS);
         assertEquals(response.getCode(), SUCCESS_CODE);
         assertEquals(response.getMessage(), OK);
-//        assertEquals(response.getData(), RESTAURANT_REST);
+        assertEquals(response.getData(), RESTAURANT_REST);
     }
 
 }
