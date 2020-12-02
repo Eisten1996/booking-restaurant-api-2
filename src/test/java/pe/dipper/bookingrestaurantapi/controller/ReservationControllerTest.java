eab92ff631b059f29f393577e49a1268bb854df2
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-02 16:58:52 -0500

    Test Unitario del Controlador de Reservas (Crear y testear el metodo)

--- src/test/java/pe/dipper/bookingrestaurantapi/controller/ReservationControllerTest.java
+++ src/test/java/pe/dipper/bookingrestaurantapi/controller/ReservationControllerTest.java
@@ -9,9 +9,15 @@ import org.mockito.MockitoAnnotations;
 import pe.dipper.bookingrestaurantapi.controllers.ReservationController;
 import pe.dipper.bookingrestaurantapi.exceptions.BookingException;
 import pe.dipper.bookingrestaurantapi.jsons.CreateReservationRest;
+import pe.dipper.bookingrestaurantapi.jsons.ReservationRest;
+import pe.dipper.bookingrestaurantapi.responses.BookingResponse;
 import pe.dipper.bookingrestaurantapi.services.ReservationService;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Dipper
@@ -31,6 +37,7 @@ public class ReservationControllerTest {
     private static final String LOCATOR = "BURGER 2";
 
     CreateReservationRest CREATE_RESERVATION_REST = new CreateReservationRest();
+    ReservationRest RESERVATION_REST = new ReservationRest();
 
     @Mock
     ReservationService reservationService;
@@ -45,12 +52,26 @@ public class ReservationControllerTest {
         CREATE_RESERVATION_REST.setPerson(PERSON);
         CREATE_RESERVATION_REST.setRestaurantId(RESTAURANT_ID);
         CREATE_RESERVATION_REST.setTurnId(TURN_ID);
-
-        Mockito.when(reservationService.createReservation(CREATE_RESERVATION_REST)).thenReturn(LOCATOR);
     }
 
     @Test
-    public void creationReservationTest() throws BookingException{
+    public void creationReservationTest() throws BookingException {
+        Mockito.when(reservationService.createReservation(CREATE_RESERVATION_REST)).thenReturn(LOCATOR);
+        final BookingResponse<String> response = reservationController.createReservation(CREATE_RESERVATION_REST);
+        assertEquals(response.getStatus(), SUCCESS_STATUS);
+        assertEquals(response.getCode(), SUCCESS_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), LOCATOR);
 
+    }
+
+    @Test
+    public void getReservationIdTest() throws BookingException {
+        Mockito.when(reservationService.getReservation(RESTAURANT_ID)).thenReturn(RESERVATION_REST);
+        final BookingResponse<ReservationRest> response = reservationController.getReservationId(RESTAURANT_ID);
+        assertEquals(response.getStatus(), SUCCESS_STATUS);
+        assertEquals(response.getCode(), SUCCESS_CODE);
+        assertEquals(response.getMessage(), OK);
+        assertEquals(response.getData(), RESERVATION_REST);
     }
 }
