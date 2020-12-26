9a29b54502bc78e75c70884c14f1a7df6ee4b5dd
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-25 23:02:39 -0500

    Crear metodo privado para el envio de emails

--- src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/EmailServiceImpl.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/EmailServiceImpl.java
@@ -1,10 +1,16 @@
 package pe.dipper.bookingrestaurantapi.services.Impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import pe.dipper.bookingrestaurantapi.exceptions.BookingException;
+import pe.dipper.bookingrestaurantapi.exceptions.InternalServerErrorException;
 import pe.dipper.bookingrestaurantapi.services.EmailService;
+
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author Dipper
@@ -15,6 +21,8 @@ import pe.dipper.bookingrestaurantapi.services.EmailService;
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailServiceImpl.class);
+
     @Autowired
     private JavaMailSender javaMailSender;
 
@@ -22,5 +30,20 @@ public class EmailServiceImpl implements EmailService {
     public String processSendEmail(String receiver, String subject, String templateCode, String currentName) throws BookingException {
 
         return null;
+    }
+
+    private void sendEmail(final String receiver, final String subject, final String template) throws BookingException {
+        final MimeMessage email = javaMailSender.createMimeMessage();
+        final MimeMessageHelper content;
+        try {
+            content = new MimeMessageHelper(email, true);
+            content.setSubject(subject);
+            content.setTo(receiver);
+            content.setText(template, true);
+        } catch (Exception e) {
+            LOGGER.error("INTERNAL_SERVER_ERROR", e);
+            throw new InternalServerErrorException("INTERNAL_SERVER_ERROR", "INTERNAL_SERVER_ERROR");
+        }
+        javaMailSender.send(email);
     }
 }
