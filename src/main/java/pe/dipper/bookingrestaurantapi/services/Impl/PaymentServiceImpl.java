cb2c5b4e24f0a44dac57e1c7b5aaaaf50e9f0f46
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-27 18:57:14 -0500

    Revisando y analizando la documentacion de Stripe

--- src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/PaymentServiceImpl.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/services/Impl/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package pe.dipper.bookingrestaurantapi.services.Impl;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Service;
 import pe.dipper.bookingrestaurantapi.jsons.PaymentConfirmRest;
 import pe.dipper.bookingrestaurantapi.jsons.PaymentIntentRest;
 import pe.dipper.bookingrestaurantapi.services.PaymentService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Dipper
@@ -19,9 +25,25 @@ public class PaymentServiceImpl implements PaymentService {
     @Value("$stripe.key.secretKey")
     String secretKey;
 
+    public enum Currency {
+        USD, EUR;
+    }
+
     @Override
     public PaymentIntent paymentIntent(PaymentIntentRest paymentIntentRest) throws StripeException {
-        return null;
+        Stripe.apiKey = secretKey;
+        Map<String, Object> params = new HashMap<>();
+        List<Object> paymentMethodTypes =
+                new ArrayList<>();
+        paymentMethodTypes.add("card");
+        params.put("amount", paymentIntentRest.getPrice());
+        params.put("currency", Currency.EUR);
+        params.put("description", paymentIntentRest.getDescription());
+        params.put(
+                "payment_method_types",
+                paymentMethodTypes
+        );
+        return PaymentIntent.create(params);
     }
 
     @Override
