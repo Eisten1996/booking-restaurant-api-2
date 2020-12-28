85f1df8ea3f1370bc47da1fdec8e1cbedbde0705
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-27 21:56:08 -0500

    Controlador de confirmar y cancelar un intento de pago

--- src/main/java/pe/dipper/bookingrestaurantapi/controllers/PaymentController.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/controllers/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.dipper.bookingrestaurantapi.jsons.PaymentConfirmRest;
 import pe.dipper.bookingrestaurantapi.jsons.PaymentIntentRest;
 import pe.dipper.bookingrestaurantapi.services.PaymentService;
 
@@ -24,9 +25,29 @@ public class PaymentController {
     PaymentService paymentService;
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/paymentIntent",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/paymentIntent", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> paymentIntent(@RequestBody PaymentIntentRest paymentIntentRest) throws StripeException {
         PaymentIntent paymentIntent = paymentService.paymentIntent(paymentIntentRest);
+
+        String paymentString = paymentIntent.toJson();
+
+        return new ResponseEntity<String>(paymentString, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/confirm", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> paymentConfirm(@RequestBody PaymentConfirmRest paymentConfirmRest) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.paymentConfirm(paymentConfirmRest);
+
+        String paymentString = paymentIntent.toJson();
+
+        return new ResponseEntity<String>(paymentString, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/cancel/" + "{paymentId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> paymentCancel(@PathVariable("paymentId") String paymentId) throws StripeException {
+        PaymentIntent paymentIntent = paymentService.paymentCancel(paymentId);
 
         String paymentString = paymentIntent.toJson();
 
