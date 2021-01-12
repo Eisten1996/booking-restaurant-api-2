a7292e75da64a615be95597d9fccdc444ac7dd33
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-01-11 22:41:51 -0500

    actualizando los cors

--- src/main/java/pe/dipper/bookingrestaurantapi/controllers/ReservationController.java
+++ src/main/java/pe/dipper/bookingrestaurantapi/controllers/ReservationController.java
@@ -17,7 +17,7 @@ import pe.dipper.bookingrestaurantapi.services.ReservationService;
  */
 
 @RestController()
-@CrossOrigin(value = "https://booking-restaurant-app.web.app:4200")
+@CrossOrigin(value = "https://151.101.65.195:4200")
 @RequestMapping(path = "/booking-restaurant" + "/v1")
 public class ReservationController {
 
